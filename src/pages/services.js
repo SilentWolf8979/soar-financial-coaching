cac57f13264dfd4012835af40c958c814bdcd4f5
SilentWolf8979 <NeilDoebler@outlook.com>
2021-02-28 16:33:29 -0600

    Fix services spacing

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,27 +4,39 @@ import { Link } from 'react-router-dom';
 function Services() {
     return (
       <div className="App">
-        <div className='pageContent'>
+        <div className='pageContent narrowMargin'>
           <h2>Services Offered</h2>
           <br />
           <Row className='justify-content-center'>
-            <Col xs={12} md={5} lg={3} className='cta ctaService'>
-              <Link to='/services/speaking'>Speaking Engagements</Link>
+            <Col xs={12} sm={6} md={4} className='mb-3'>
+              <div className='cta ctaService'>
+              <Link to='/services/speaking' className='title'>Speaking Engagements</Link>
+              </div>
             </Col>
-            <Col xs={12} md={5} lg={3} className='cta ctaService'>
-              <Link to='/services/debt-elimination'>Debt Elimination</Link>
+            <Col xs={12} sm={6} md={4} className='mb-3'>
+            <div className='cta ctaService'>
+              <Link to='/services/debt-elimination' className='title'>Debt Elimination</Link>
+              </div>
             </Col>
-            <Col xs={12} md={5} lg={3} className='cta ctaService'>
-              <Link to='/services/money-and-marriage'>Money and Marriage</Link>
+            <Col xs={12} sm={6} md={4} className='mb-3'>
+            <div className='cta ctaService'>
+              <Link to='/services/money-and-marriage' className='title'>Money and Marriage</Link>
+              </div>
             </Col>
-            <Col xs={12} md={5} lg={3} className='cta ctaService'>
-              <Link to='/services/launching-into-adulthood'>Launching Into Adulthood</Link>
+            <Col xs={12} sm={6} md={4} className='mb-3'>
+            <div className='cta ctaService'>
+              <Link to='/services/launching-into-adulthood' className='title'>Launching Into Adulthood</Link>
+              </div>
             </Col>
-            <Col xs={12} md={5} lg={3} className='cta ctaService'>
-              <Link to='/services/custom'>Custom</Link>
+            <Col xs={12} sm={6} md={4} className='mb-3'>
+            <div className='cta ctaService'>
+              <Link to='/services/custom' className='title'>Custom</Link>
+              </div>
             </Col>
-            <Col xs={12} md={5} lg={3} className='cta ctaService'>
-              <Link to='/services/fpu-classes'>Financial Peace University Classes</Link>
+            <Col xs={12} sm={6} md={4} className='mb-3'>
+            <div className='cta ctaService'>
+              <Link to='/services/fpu-classes' className='title'>Financial Peace University Classes</Link>
+              </div>
             </Col>
           </Row>
         </div>
